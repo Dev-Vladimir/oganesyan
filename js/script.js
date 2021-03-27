741e04809b84c210d3349cc6989dce0ae12c363c
Dev-Vladimir <63652808+Dev-Vladimir@users.noreply.github.com>
2021-03-28 01:44:32 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 
 let burger = document.querySelector('.burger');
 	menu = document.querySelector('.main-screen-menu');
-	parentUl = false;
 burger.onclick = function(){
 	this.classList.toggle('active');
 	menu.classList.toggle('active');
@@ -12,35 +11,81 @@ menuItems.forEach(item => {
 	item.addEventListener('click', showSubMenu)
 })
 
+// function showSubMenu(e){
+// 	e.preventDefault();
+// 	let item = e.target;
+// 		subs = item.parentNode.querySelector('ul');
+// 	if (subs !== null){
+// 		subs = subs.cloneNode(true);
+// 		let parent = item.closest('ul');
+// 		let back = document.createElement('span');
+
+// 		back.classList.add('back');
+// 		back.innerHTML = '❮ Назад';
+// 		back.addEventListener('click', showParentUl);
+// 		menu.prepend(back);
+// 		parentUl = parent;
+// 		parent.replaceWith(subs);
+// 	}else{
+// 		console.log(1);
+// 		document.location.href = item.href;
+// 	}
+// 	console.log(item.href);
+
+// }
+
+// function showParentUl(e){
+// 	let elem = e.target;
+	
+// 		ul = menu.querySelector('ul');
+// 		console.log(parent);
+
+// 	ul.replaceWith(parentUl);
+// 	menu.removeChild(elem);
+// }
+
 function showSubMenu(e){
 	e.preventDefault();
 	let item = e.target;
 		subs = item.parentNode.querySelector('ul');
+
 	if (subs !== null){
 		subs = subs.cloneNode(true);
 		let parent = item.closest('ul');
-		let back = document.createElement('span');
-
+		subs = subs.cloneNode(true);
+		// console.log(menu);
+		let back = document.createElement('li');
 		back.classList.add('back');
-		back.innerHTML = '❮ Назад';
+		back.innerHTML = 'Назад';
+		let span = document.createElement('span');
+		span.innerHTML = '❮'
+		back.prepend(span);
+		subs.prepend(back);
+		subs.classList.add('current');
+		subs.style.opacity = '0';
+		setTimeout(function(){
+			subs.style.transform = "translateX(0%)";
+			subs.style.opacity = '1';
+		}, 300);
+		menu.appendChild(subs)
+		parent.style.transform = 'translateX(-100%)';
+		parent.style.opacity = '0';
 		back.addEventListener('click', showParentUl);
-		menu.prepend(back);
-		parentUl = parent
-		parent.replaceWith(subs);
 	}else{
-		console.log(1);
+		// console.log(1);
 		document.location.href = item.href;
 	}
-	console.log(item.href);
-
 }
 
 function showParentUl(e){
-	let elem = e.target;
-	
-		ul = menu.querySelector('ul');
-		console.log(parent);
-
-	ul.replaceWith(parentUl);
-	menu.removeChild(elem);
+	let current = menu.querySelector('.current');
+		primary = menu.querySelector('ul');
+	console.log(primary);
+	current.style.transform = 'scale(3)';
+	current.style.opacity = '0';
+	setTimeout(function(){
+			primary.style.transform = "translateX(0%)";
+			primary.style.opacity = '1';
+			menu.removeChild(current);
+		}, 300);
 }
